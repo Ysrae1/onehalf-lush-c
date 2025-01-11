4fcd7e4c3bfd7b3ed0ca851047ad3874cc0aac7c
ysrae1 <ysrae1@outlook.com>
2025-01-11 18:17:35 +0800

    Update colors.lua

--- lua/onehalf-lush/colors.lua
+++ lua/onehalf-lush/colors.lua
@@ -33,9 +33,9 @@ M.dark = {
   red     = "#e06c75",
   green   = "#98c379",
   yellow  = "#e5c07b",
-  blue    = "#61afef",
+  blue    = "#70bfff",
   purple  = "#c678dd",
-  cyan    = "#106A79",
+  cyan    = "#105069",
   white   = "#dcdfe4",
 
   comment_fg  = "#5c6370",
