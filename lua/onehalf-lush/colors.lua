0c0efdd0847d44cdc1f01a396c276916301ace6a
ysrae1 <ysrae1@outlook.com>
2025-01-11 18:52:23 +0800

    Update colors.lua

--- lua/onehalf-lush/colors.lua
+++ lua/onehalf-lush/colors.lua
@@ -33,9 +33,9 @@ M.dark = {
   red     = "#e06c75",
   green   = "#98c379",
   yellow  = "#e5c07b",
-  blue    = "#80d4ff",
+  blue    = "#7fbfff",
   purple  = "#c678dd",
-  cyan    = "#165970",
+  cyan    = "#1c4588",
   white   = "#dcdfe4",
 
   comment_fg  = "#5c6370",
